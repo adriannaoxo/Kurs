24b8a9ebc0cde0a5107b3953809a4cd73972a6e7
adriannaoxo <48280963+adriannaoxo@users.noreply.github.com>
2019-03-06 12:09:31 +0100

    Add files via upload

---
+++ Metody.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class Metody{
+  public static void main(String[] args) {
+    System.out.print("Podaj temperaturę ");
+    Scanner scan = new Scanner(System.in);
+    double tempreture = scan.nextDouble();
+    tempCheck(tempreture);
+  }
+  private static void tempCheck (double tempreture){
+    if(tempreture>0){
+      System.out.println(tempreture + " to temperatura dodatnia");
+    }
+    else{
+      System.out.println(tempreture + " to temperatura ujemna");
+    }
+  }
+}
